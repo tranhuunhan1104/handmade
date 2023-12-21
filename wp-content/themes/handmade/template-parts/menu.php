9cc83068bf8b210f95f90e9d323fd89caed01d34
tranhuunhan1104 <tranhuunhan1104@gmail.com>
2023-12-21 16:45:51 +0700

    thirty commit

--- wp-content/themes/handmade/template-parts/menu.php
+++ wp-content/themes/handmade/template-parts/menu.php
@@ -20,61 +20,206 @@ $args = array(
 );
 
 $terms = get_terms($args);
-
 // Kiểm tra xem có category nào hay không
 if ($terms) {
-    // Lặp qua từng category và hiển thị thông tin
+    ?>
+  <li class="nav-item">
+    <a class="nav-link active show" data-bs-toggle="tab" data-bs-target="#<?php echo $terms[0]->name; ?>">
+      <h4><?php echo $terms[0]->name; ?></h4>
+    </a>
+  </li><!-- End tab nav item -->
+
+<?php
+// Lặp qua từng category và hiển thị thông tin
+    $skipFirst = true;
     foreach ($terms as $category) {
+        if ($skipFirst) {
+            $skipFirst = false;
+            continue; // Bỏ qua phần tử đầu tiên
+        }
         ?>
-                                        <li class="nav-item">
-                                        <a class="nav-link active show" data-bs-toggle="tab" data-bs-target="#<?php echo $category->name ?>">
-                                          <h4><?php echo $category->name ?></h4>
-                                        </a>
-                                        </li><!-- End tab nav item -->
-                                        <?php
+                  <li class="nav-item">
+                    <a class="nav-link" data-bs-toggle="tab" data-bs-target="#<?php echo $category->name; ?>">
+                      <h4><?php echo $category->name; ?></h4>
+                    </a>
+                  </li><!-- End tab nav item -->
+
+        <?php
 
     }
+
 }
 ?>
-
-
-
         </ul>
 
         <div class="tab-content" data-aos="fade-up" data-aos-delay="300">
-<?php
-if ($terms) {
-    // Lặp qua từng category và hiển thị thông tin
-    foreach ($terms as $category) {
-        ?>
-      <div class="tab-pane fade active show" id="<?php echo $category->name ?>">
+           <div class="tab-pane fade active show" id="<?php echo $terms[0]->name; ?>">
 
-      <div class="tab-header text-center">
-        <p>Menu</p>
-        <h3><?php echo $category->name ?></h3>
-      </div>
+                <div class="tab-header text-center">
+                  <p>Menu</p>
+                  <h3><?php echo $terms[0]->name; ?></h3>
+                </div>
 
-      <div class="row gy-5">
+                <div class="row gy-5">
 
-        <div class="col-lg-4 menu-item">
-          <a href="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-1.png" class="glightbox"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-1.png" class="menu-img img-fluid" alt=""></a>
-          <h4>Magnam Tiste</h4>
-          <p class="ingredients">
-            Lorem, deren, trataro, filede, nerada
-          </p>
-          <p class="price">
-            $5.95
-          </p>
-        </div><!-- Menu Item -->
+                  <div class="col-lg-4 menu-item">
+                    <a href="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-1.png" class="glightbox"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-1.png" class="menu-img img-fluid" alt=""></a>
+                    <h4>Magnam Tiste</h4>
+                    <p class="ingredients">
+                      Lorem, deren, trataro, filede, nerada
+                    </p>
+                    <p class="price">
+                      $5.95
+                    </p>
+                  </div><!-- Menu Item -->
 
-      </div>
-    </div><!-- End Starter Menu Content -->
-    <?php
-}
+                  <div class="col-lg-4 menu-item">
+                    <a href="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-2.png" class="glightbox"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-2.png" class="menu-img img-fluid" alt=""></a>
+                    <h4>Aut Luia</h4>
+                    <p class="ingredients">
+                      Lorem, deren, trataro, filede, nerada
+                    </p>
+                    <p class="price">
+                      $14.95
+                    </p>
+                  </div><!-- Menu Item -->
+
+                  <div class="col-lg-4 menu-item">
+                    <a href="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-3.png" class="glightbox"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-3.png" class="menu-img img-fluid" alt=""></a>
+                    <h4>Est Eligendi</h4>
+                    <p class="ingredients">
+                      Lorem, deren, trataro, filede, nerada
+                    </p>
+                    <p class="price">
+                      $8.95
+                    </p>
+                  </div><!-- Menu Item -->
+
+                  <div class="col-lg-4 menu-item">
+                    <a href="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-4.png" class="glightbox"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-4.png" class="menu-img img-fluid" alt=""></a>
+                    <h4>Eos Luibusdam</h4>
+                    <p class="ingredients">
+                      Lorem, deren, trataro, filede, nerada
+                    </p>
+                    <p class="price">
+                      $12.95
+                    </p>
+                  </div><!-- Menu Item -->
+
+                  <div class="col-lg-4 menu-item">
+                    <a href="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-5.png" class="glightbox"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-5.png" class="menu-img img-fluid" alt=""></a>
+                    <h4>Eos Luibusdam</h4>
+                    <p class="ingredients">
+                      Lorem, deren, trataro, filede, nerada
+                    </p>
+                    <p class="price">
+                      $12.95
+                    </p>
+                  </div><!-- Menu Item -->
+
+                  <div class="col-lg-4 menu-item">
+                    <a href="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-6.png" class="glightbox"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-6.png" class="menu-img img-fluid" alt=""></a>
+                    <h4>Laboriosam Direva</h4>
+                    <p class="ingredients">
+                      Lorem, deren, trataro, filede, nerada
+                    </p>
+                    <p class="price">
+                      $9.95
+                    </p>
+                  </div><!-- Menu Item -->
+
+                </div>
+          </div><!-- End Starter Menu Content -->
+        <?php
+// Lặp qua từng category và hiển thị thông tin
+$skipFirst = true;
+foreach ($terms as $category) {
+    if ($skipFirst) {
+        $skipFirst = false;
+        continue; // Bỏ qua phần tử đầu tiên
+    }
+    ?>
+          <div class="tab-pane fade" id="<?php echo $category->name; ?>">
+
+            <div class="tab-header text-center">
+              <p>Menu</p>
+              <h3><?php echo $category->name; ?></h3>
+            </div>
+
+            <div class="row gy-5">
+
+              <div class="col-lg-4 menu-item">
+                <a href="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-1.png" class="glightbox"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-1.png" class="menu-img img-fluid" alt=""></a>
+                <h4>Magnam Tiste</h4>
+                <p class="ingredients">
+                  Lorem, deren, trataro, filede, nerada
+                </p>
+                <p class="price">
+                  $5.95
+                </p>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-2.png" class="glightbox"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-2.png" class="menu-img img-fluid" alt=""></a>
+                <h4>Aut Luia</h4>
+                <p class="ingredients">
+                  Lorem, deren, trataro, filede, nerada
+                </p>
+                <p class="price">
+                  $14.95
+                </p>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-3.png" class="glightbox"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-3.png" class="menu-img img-fluid" alt=""></a>
+                <h4>Est Eligendi</h4>
+                <p class="ingredients">
+                  Lorem, deren, trataro, filede, nerada
+                </p>
+                <p class="price">
+                  $8.95
+                </p>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-4.png" class="glightbox"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-4.png" class="menu-img img-fluid" alt=""></a>
+                <h4>Eos Luibusdam</h4>
+                <p class="ingredients">
+                  Lorem, deren, trataro, filede, nerada
+                </p>
+                <p class="price">
+                  $12.95
+                </p>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-5.png" class="glightbox"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-5.png" class="menu-img img-fluid" alt=""></a>
+                <h4>Eos Luibusdam</h4>
+                <p class="ingredients">
+                  Lorem, deren, trataro, filede, nerada
+                </p>
+                <p class="price">
+                  $12.95
+                </p>
+              </div><!-- Menu Item -->
+
+              <div class="col-lg-4 menu-item">
+                <a href="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-6.png" class="glightbox"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/menu/menu-item-6.png" class="menu-img img-fluid" alt=""></a>
+                <h4>Laboriosam Direva</h4>
+                <p class="ingredients">
+                  Lorem, deren, trataro, filede, nerada
+                </p>
+                <p class="price">
+                  $9.95
+                </p>
+              </div><!-- Menu Item -->
+
+            </div>
+          </div><!-- End Starter Menu Content -->
+          <?php
 }
 
 ?>
-
 
         </div>
 
